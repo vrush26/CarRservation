e67b31f9402f20fe5094ed9961a8ee1929168dce
vrush26 <fasatevrushali@gmail.com>
2025-07-17 12:20:42 +0200

    Test file created

---
+++ src/main/java/test/CarReservationTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.junit.jupiter.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class CarReservationTest {
+
+    @Test
+    public  void test(){
+
+    }
+}
+
